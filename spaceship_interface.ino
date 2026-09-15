7058e23f32aefd1904966bc079f66525a0c0ddd9
John Syver Lien <81336862+Nu11ify@users.noreply.github.com>
2026-09-15 12:09:21 +0200

    Update spaceship_interface.ino
    Modified to fire SOS on green leds each 3rd run of red leds

--- spaceship_interface.ino
+++ spaceship_interface.ino
@@ -1,4 +1,5 @@
 int switchState = 0;
+int iCounter=0;
 
 void setup() {
   pinMode(3,OUTPUT);
@@ -11,18 +12,77 @@ void loop() {
   switchState = digitalRead(2);
 
   if (switchState == LOW) {
+    //Resets counter so it runs 3 passes between each button press
+    iCounter=0;
     digitalWrite(3, HIGH);
     digitalWrite(4, HIGH);
     digitalWrite(5, HIGH);
-  }
+  } 
   else {
     digitalWrite(3, LOW);
     digitalWrite(4, LOW);
-    digitalWrite(5, HIGH);
+    if (iCounter==3){sosAlert();}
 
+    digitalWrite(4, LOW);
+    digitalWrite(5, HIGH);
     delay(250);
     digitalWrite(4, HIGH);
     digitalWrite(5, LOW);
     delay(250);
+    iCounter+=1;
   }
+}
+
+//Triggers SOS on green led
+void sosAlert() {
+  //S
+    digitalWrite(3, HIGH);
+    delay(100);
+    digitalWrite(3, LOW);
+    delay(100);
+
+    digitalWrite(3, HIGH);
+    delay(100);
+    digitalWrite(3, LOW);
+    delay(100);
+
+    digitalWrite(3, HIGH);
+    delay(100);
+    digitalWrite(3, LOW);
+    delay(100);
+
+    //O
+    digitalWrite(3, HIGH);
+    delay(200);
+    digitalWrite(3, LOW);
+    delay(200);
+
+    digitalWrite(3, HIGH);
+    delay(200);
+    digitalWrite(3, LOW);
+    delay(200);
+
+    digitalWrite(3, HIGH);
+    delay(200);
+    digitalWrite(3, LOW);
+    delay(200);
+
+    //S
+    digitalWrite(3, HIGH);
+    delay(100);
+    digitalWrite(3, LOW);
+    delay(100);
+
+    digitalWrite(3, HIGH);
+    delay(100);
+    digitalWrite(3, LOW);
+    delay(100);
+
+    digitalWrite(3, HIGH);
+    delay(100);
+    digitalWrite(3, LOW);
+    delay(100);
+
+    //Resets counter
+    iCounter=0;
 }
